49a02a94b06ba686707eeb426d37d6e113f8945b
Ahmad Ali Khan <ahmadk.net@gmail.com>
2026-05-25 20:03:08 +0530

    Add project files.

---
+++ TrainingManagement.CourseApi/Data/Course.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrainingManagement.CourseApi.Data
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+        public string CourseName { get; set; } = string.Empty;
+        public int CourseDescription { get; set; }
+        public int Duration { get; set; }
+
+    }
+}
---
+++ TrainingManagement.CourseApi/Models/CourseDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace TrainingManagement.CourseApi.Models
+{
+    public class CourseDbContext
+    {
+    }
+}
